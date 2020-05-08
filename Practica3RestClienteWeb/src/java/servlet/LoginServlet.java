26fd410129adb1b9d39e57ac2cca36c8cda8503c
asombrosio <asombrosio89@gmail.com>
2020-05-08 17:22:34 +0200

    Retocada LoginServlet

--- Practica3RestClienteWeb/src/java/servlet/LoginServlet.java
+++ Practica3RestClienteWeb/src/java/servlet/LoginServlet.java
@@ -7,6 +7,7 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,22 +28,7 @@ public class LoginServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -56,7 +42,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
@@ -70,7 +56,21 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String nombre = request.getParameter("nombre");
+        String password = request.getParameter("password");
+        
+        if(Validar.ValidarUsuario(nombre, password)){
+            RequestDispatcher validacion = request.getRequestDispatcher("Bienvenido"); //Ir al menu o algo asi
+            validacion.forward(request, response);
+        } else{
+                out.println("Nombre de usuario o password incorrecto");
+                RequestDispatcher error = request.getRequestDispatcher("index.html"); //o el index o error sql
+                error.forward(request, response);
+        }
     }
 
     /**
