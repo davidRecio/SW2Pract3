3edd96f6c00928eebf10ca02c9d46e49247c7af0
David Recio Arnés <davidrecioarnes@gmail.com>
2020-05-13 15:41:26 +0200

    servlet log y validar, falta hacer q el log te llama a la funcion log ok

--- Practica3RestClienteWeb/src/java/servlet/ValidarXSD.java
+++ Practica3RestClienteWeb/src/java/servlet/ValidarXSD.java
@@ -5,7 +5,7 @@
  */
 package servlet;
 
-import beans.Usuario;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -15,9 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author darth
+ * @author david
  */
-public class LoginServlet extends HttpServlet {
+public class ValidarXSD extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -30,46 +30,24 @@ public class LoginServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+             
         response.setContentType("text/html;charset=UTF-8");
-        
-        String nombre = request.getParameter("nombre");
-        String password = request.getParameter("password");
-        String respuesta = "";
-        
-        Modelo modelo = new Modelo();
-        Usuario user = new Usuario();
-        
-            user.setNombre(nombre);
-            user.setPassword(password);
-            modelo.setIdUsuario(modelo.validarUsuario(user));
-            if(modelo.getIdUsuario() <0){
-                respuesta = "Usuario invalido";
-            
-            }else{
- 
-               respuesta = "Usuario valido";
-               
-            }
-//        if(Validar.ValidarUsuario(nombre, password)){
-//            RequestDispatcher validacion = request.getRequestDispatcher("Bienvenido"); //Ir al menu o algo asi
-//            validacion.forward(request, response);
-//        } else{
-//                out.println("Nombre de usuario o password incorrecto");
-//                RequestDispatcher error = request.getRequestDispatcher("index.html"); //o el index o error sql
-//                error.forward(request, response);
-//        }
+          String rutaFichero = request.getParameter("fichero"); 
+        Modelo mod = new Modelo();
+        String valor=mod.validarXSD(new File(rutaFichero));
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("<title>Servlet validarXSD</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>" + modelo.validarUsuario(user) + "</h1>");
+            out.println("<h1>" + valor + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
+    
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
